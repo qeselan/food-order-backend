86782ab73beb4a886902e9aba0ab0c1083085c29
Omer Taniyici <taniyici.omer@gmail.com>
2023-11-12 09:06:36 -0500

    refactor index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import path from 'path';
+import App from './services/ExpressApp';
+import dbConnection from './services/Database';
 
-import { AdminRoute, VandorRoute } from './routes';
-import { MONGO_URI } from './config';
+const StartServer = async () => {
+  const app = express();
+  await dbConnection();
+  await App(app);
+  app.listen(8000, () => {
+    console.log('Listening to port 8000');
+  });
+};
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.use('/admin', AdminRoute);
-app.use('/vandor', VandorRoute);
-
-mongoose
-  .connect(MONGO_URI, {})
-  .then((result) => {
-    // console.log(result);
-    console.log('DB connected');
-  })
-  .catch((err) => console.log('error: ' + err));
-
-app.listen(8000, () => {
-  console.clear();
-  console.log('App is listening to the port 8000');
-});
+StartServer();
---
+++ services/Database.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../config';
+
+export default async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {});
+
+    console.clear();
+    console.log('DB Connected...');
+  } catch (error) {
+    console.log(error);
+  }
+};
